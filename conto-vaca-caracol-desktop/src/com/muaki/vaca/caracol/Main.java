859237c60140cf1c073cae7376c2ca7ed4e45926
butelo <butelin@gmail.com>
2013-06-21 18:46:52 +0200

    ai che vai esta

--- conto-vaca-caracol-desktop/src/com/muaki/vaca/caracol/Main.java
+++ conto-vaca-caracol-desktop/src/com/muaki/vaca/caracol/Main.java
@@ -6,11 +6,16 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 public class Main {
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-		cfg.title = "conto-vaca-caracol";
 		cfg.useGL20 = true;
+		cfg.samples=3;
 		cfg.width = 480;
 		cfg.height = 320;
+		cfg.vSyncEnabled= true;
 		
 		new LwjglApplication(new VaCaracol(), cfg);
+
+		
+		
+		
 	}
 }
